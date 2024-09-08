30f498857aaad0a46c3b8ed8f971bc99f9456b34
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2024-09-08 19:46:41 -0400

    removing celery

--- HotelManagement/Inventory/tasks.py
+++ HotelManagement/Inventory/tasks.py
@@ -1,24 +1,24 @@
-from celery import shared_task
-from Inventory.models import Inventory
-from Inventory.serializers import InventorySerializer
-import traceback
+# from celery import shared_task
+# from Inventory.models import Inventory
+# from Inventory.serializers import InventorySerializer
+# import traceback
 
-@shared_task
-def get_roominventory_data():
-    try:
-        items = Inventory.objects.filter(is_roominventory=True)
-        inventory_serializer = InventorySerializer(items, many=True)
+# @shared_task
+# def get_roominventory_data():
+#     try:
+#         items = Inventory.objects.filter(is_roominventory=True)
+#         inventory_serializer = InventorySerializer(items, many=True)
 
-        return {
-            "status": "success",
-            "message": "ITEMS_FETCHED_SUCCESSFULLY",
-            "data": inventory_serializer.data
-        }
+#         return {
+#             "status": "success",
+#             "message": "ITEMS_FETCHED_SUCCESSFULLY",
+#             "data": inventory_serializer.data
+#         }
 
-    except Exception as e:
+#     except Exception as e:
 
-        traceback.print_exc()
-        return {
-            "status": "error",
-            "message": "An error occurred while fetching room inventory data."
-        }
+#         traceback.print_exc()
+#         return {
+#             "status": "error",
+#             "message": "An error occurred while fetching room inventory data."
+#         }
